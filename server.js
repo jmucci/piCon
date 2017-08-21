6aee76d6ef0fb942797253c0ebae6960a710f9f5
John Mucci <jmucci@oasisHood.com>
2017-08-21 09:48:44 -0400

    again

--- server.js
+++ server.js
@@ -21,6 +21,18 @@ var omx = require('omxdirector');
 
 var iTach = require('./my-simple-itach');  // my home grown version
 
+var rpi433    = require('rpi-433'),
+rfSniffer = rpi433.sniffer({
+  pin: 2,                     //Snif on GPIO 2 (or Physical PIN 13) 
+  debounceDelay: 500          //Wait 500ms before reading another code 
+}),
+rfEmitter = rpi433.emitter({
+  pin: 17,                     //Send through GPIO 0 (or Physical PIN 11) 
+  pulseLength: 350            //Send the code with a 350 pulse length 
+});
+
+
+
 // var smartcast = require('vizio-smart-cast');
 var exec = require('child_process').exec;
 var needle = require('needle');
@@ -170,6 +182,14 @@ function sendRFCommand(code, p, t)
         }
     });
 }
+
+
+function xxxsendRFCommand(code, p, t)
+{
+	rfEmitter.sendCode(code, {pulseLength: p});
+}
+
+
 // find items by ID
 function myFindId(list, id)
 {
